37c6aeae8a64138c1470d152106115c5a5c8aac7
Alejandro Hernandez <alejandroh3005@gmail.com>
2022-05-04 13:24:15 -0700

    fixed comment spacing

--- rootlocusplot.py
+++ rootlocusplot.py
@@ -2,52 +2,55 @@ import numpy as np
 import sympy as sp
 
 class RLPlotting:
-    def getPoles(self): #the poles of a controller-plant system are defined as the roots of the denominator of L(s)
+    def getPoles(self):  # the poles of a controller-plant system are defined as the roots of the denominator of L(s)
         self.n = len(self.p)
         return np.roots(self.denominator)
 
-    def getZeroes(self): #the zeroes of a controller-plant system are defined as the roots of the numerator of L(s)
+    def getZeroes(self):  # the zeroes of a controller-plant system are defined as the roots of the numerator of L(s)
         self.n = len(self.z)
         return np.roots(self.numerator)
 
-    def getAsymptote(self): #this is the first half of Rule 3: provided the point on the Real Axis that the asymptote intersects
+    def getAsymptote(
+            self):  # this is the first half of Rule 3: provided the point on the Real Axis that the asymptote intersects
         return (np.sum(self.p) - np.sum(self.z)) / (self.n - self.m)
 
-    def getThetas(self): #this is Part 2 of Rule 3: the angles of the asymptotes with the Real Axis
+    def getThetas(self):  # this is Part 2 of Rule 3: the angles of the asymptotes with the Real Axis
         theta = [np.pi * (2 * i - 1) / (self.n - self.m) for i in range(self.n - self.m)]
         return theta
 
-    def getPhis(self): #this is part 1 of rule 4: phi is defined as the departure angle that a branch forms with the horizontal as it leaves a pole
+    def getPhis(
+            self):  # this is part 1 of rule 4: phi is defined as the departure angle that a branch forms with the horizontal as it leaves a pole
         phis = []
         poles = self.p
         pz = 0
         pp = 0
         for pole in poles:
             for z in self.z:
-                #sum all pole-z angles
-                pz += np.angle(pole - z, deg = True)
+                # sum all pole-z angles
+                pz += np.angle(pole - z, deg=True)
             for p in self.p:
-                #sum all pole-p angles
-                pp += np.angle(pole - p, deg = True)
-            #add difference of the sums +- 180 to phi
-            phis.append(pz - pp) #TODO +-180 check
+                # sum all pole-p angles
+                pp += np.angle(pole - p, deg=True)
+            # add difference of the sums +- 180 to phi
+            phis.append(pz - pp)  # TODO +-180 check
             pz = pp = 0
         return phis
 
-    def getPsis(self): #this is part 2 of rule 4: psi is defined as the arrival angle a branch forms with the horizontal as it enters a zero
+    def getPsis(
+            self):  # this is part 2 of rule 4: psi is defined as the arrival angle a branch forms with the horizontal as it enters a zero
         psis = []
         zeros = self.z
         zp = 0
         zz = 0
         for zero in zeros:
             for p in self.p:
-                #sum all zero-z angles
-                zp += np.angle(zero - p, deg = True)
+                # sum all zero-z angles
+                zp += np.angle(zero - p, deg=True)
             for z in self.z:
-                #sum all pole-p angles
-                zz += np.angle(zero - z, deg = True)
-            #add difference of the sums +- 180 to psis
-            psis.append(zp - zz) #TODO +- 180 check
+                # sum all pole-p angles
+                zz += np.angle(zero - z, deg=True)
+            # add difference of the sums +- 180 to psis
+            psis.append(zp - zz)  # TODO +- 180 check
             zp = zz = 0
         return psis
 
@@ -60,7 +63,7 @@ class RLPlotting:
             d += coeff * s ** (self.n - i)
         L = n / d
         derivL = L.diff(s)
-        breakpoints = sp.nroots(derivL, n = 4) 
+        breakpoints = sp.nroots(derivL, n=4)
         return breakpoints
 
     def __init__(self, numerator, denominator):
@@ -77,7 +80,7 @@ class RLPlotting:
         self.bp = self.getBreakPoints()
 
     def __str__(self):
-        pass #TODO print all the things
+        pass  # TODO print all the things
         num = f"User input numerator coefficients: {self.numerator}\n"
         den = f"User input denominator coefficients: {self.denominator}\n"
         m = f"Number of zeros (m): {self.m}\n"
@@ -90,7 +93,8 @@ class RLPlotting:
         psis = f"Branch Arrival Angles (\u03C8): {self.psis}\n"
         bp = f"Breakout Points: {self.bp}\n"
         return num + den + m + n + p + z + alpha + theta + phis + psis + bp
-    
+
+
 if __name__ == "__main__":
     n = input("Enter numerator coefficients using space as seperator: ")
     numerator = n.split()
@@ -98,4 +102,3 @@ if __name__ == "__main__":
     denominator = d.split()
     RL = RLPlotting(numerator, denominator)
     print(RL)
-    
